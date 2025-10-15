e776a2df3f2303fc0b2ef46f31b13bfa10ccc8e6
DavidHartUlea <duchenna59@gmail.com>
2025-10-15 14:49:00 +0100

    cron jobs

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import giveawayRoutes from './routes/giveawayRoutes.js';
 import giveawayModel from './models/giveawayModel.js';
-
+import job from './lib/cron.js';
 dotenv.config();
 
 const app = express();
@@ -14,6 +14,7 @@ console.log("Loaded PORT from .env:", process.env.PORT);
 // Middleware
 app.use(cors());
 app.use(express.json());
+job.start();
 
 // Initialize database
 await giveawayModel.initDB();
